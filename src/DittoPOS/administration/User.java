be7634ff4396ad7e22c13be3ab1b5400bb836a03
Ahmed Saaif <ssaaifahmed@gmail.com>
2019-06-06 14:15:52 +0800

    image

--- src/DittoPOS/administration/User.java
+++ src/DittoPOS/administration/User.java
@@ -6,6 +6,18 @@ import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Random;
 
+
+import java.io.IOException;
+
+import sun.misc.BASE64Encoder;
+import sun.misc.BASE64Decoder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 public class User {
 
     /*
@@ -35,8 +47,28 @@ public class User {
         return image;
     }
 
+    public static String encodeToString(BufferedImage image, String type) {
+        String imageString = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        try {
+            ImageIO.write(image, type, bos);
+            byte[] imageBytes = bos.toByteArray();
+
+            BASE64Encoder encoder = new BASE64Encoder();
+            imageString = encoder.encode(imageBytes);
+
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageString;
+    }
+
     public void setImage(String image) {
         this.image = image;
+
+
     }
 
     public Permissions getPermission() {
